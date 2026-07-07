0b783a78f75a83a31030923382f5e06d89364910
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-07-07 02:32:49 +0000

    Obsolete branch

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,4 @@
-if (requireNamespace("testthat", quietly = TRUE)) {
-  library(testthat)
-  if (requireNamespace("rstudioapi", quietly = TRUE)) {
-    library(nonnest2)
-    test_check("nonnest2")
-  } else {
-    message("rstudioapi package not available, skipping tests.")
-  }
-} else {
-  message("testthat package not available, skipping tests.")
-}
+library(testthat)
+library(nonnest2)
+
+test_check("nonnest2")
