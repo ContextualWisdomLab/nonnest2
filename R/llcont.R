ccdce154fc01340e7c0268764a27913eef480d8a
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-09-13 04:21:25 +0000

    Refactor ifelse to vectorized subsetting in llcont.glm binomial family
    Vectorized subsetting avoids the overhead of evaluating both true and false
    branches fully before subsetting, offering significantly better performance
    while preserving the exact same logical output.

--- R/llcont.R
+++ R/llcont.R
@@ -53,12 +53,18 @@ llcont.glm <- function(x, ...){
              if(is.matrix(y)) {
                ## Bolt: replaced apply(..., 1, sum) with optimized rowSums() for performance
                n <- rowSums(y)
-               y <- ifelse(n == 0, 0, y[, 1]/n)
+               ## Bolt: replaced ifelse with preallocation and vectorized subsetting for performance
+               res_y <- y[, 1]/n
+               res_y[n == 0] <- 0
+               y <- res_y
              } else {
                n <- rep.int(1, length(y))
              }
              m <- if (any(n > 1)) n else wt
-             wt <- ifelse(m > 0, (wt/m), 0)
+             ## Bolt: replaced ifelse with preallocation and vectorized subsetting for performance
+             res_wt <- wt/m
+             res_wt[m <= 0] <- 0
+             wt <- res_wt
              dbinom(round(m * y), round(m), mpreds, log = TRUE) * wt
            },
            quasibinomial = {
